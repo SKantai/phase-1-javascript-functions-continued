0060df1ae52920efa84522179853b5c3c1fb1370
Sayialel <sayialelkantai7@gmail.com>
2024-02-17 10:09:55 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return`This Saturday, I want to ${activity}!`
+    }
+    console.log (saturdayFun());
+    console.log (saturdayFun("bathe my dog"));
+    
+function mondayWork(activity1 = "go to the office"){
+    return `This Monday, I will ${activity1}.`
+    }
+    console.log(mondayWork());
+    console.log(mondayWork('work from home'));
+    
+function wrapAdjective (flair ='*'){
+    return function (adjective = "special"){
+    return `You are ${flair}${adjective}${flair}!`
+}}
+    wrapAdjective()
+    wrapAdjective("~")("awesome");
